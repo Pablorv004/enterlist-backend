6fe560e38782090ae254673151ff2c7005beeeba
Pablorv004 <pablorv004@gmail.com>
2025-05-26 02:51:19 +0300

    allowing this for android studio to work

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   // Configure CORS with specific options for security
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL, 'http://localhost:5173', 
+      process.env.FRONTEND_URL, 'http://localhost:5173', 'https://localhost',
       'https://accounts.spotify.com',
       'https://api.spotify.com',
       'https://accounts.google.com',
