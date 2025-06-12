509a2cdac5a90f5c0ed28e12302702b1e2ba865f
Pablorv004 <pablorv004@gmail.com>
2025-06-13 01:01:35 +0200

    correct dependency import in the transactions module

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,9 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PaypalAuthModule } from '../paypal-auth/paypal-auth.module';
+import { SubmissionsModule } from '../submissions/submissions.module';
 
 @Module({
-  imports: [HttpModule, PaypalAuthModule],
+  imports: [HttpModule, PaypalAuthModule, SubmissionsModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
